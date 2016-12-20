6aa13df6d22178e6f0c593192b9bc30cbfd293e6
Oliver Hoogvliet <oliver.hoogvliet@codecentric.de>
2016-12-20 17:25:22 +0100

    Replace bash script by ansible playbook

--- geographicalHierarchyGraphServer/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get upgrade -y
-apt-get install docker.io -y
-sudo usermod -a -G docker vagrant
-mkdir -p /app/logs /app/config /app/neo4j/data /app/neo4j/logs
-usermod -aG docker vagrant
