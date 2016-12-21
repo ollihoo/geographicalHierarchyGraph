cd025d048d7300ff31cf32ca31e1c1d95e88ac47
Oliver Hoogvliet <oliver.hoogvliet@codecentric.de>
2016-12-21 11:35:06 +0100

    Add possibility to see version on homepage

--- src/main/groovy/de/ollihoo/controller/IndexController.groovy
+++ src/main/groovy/de/ollihoo/controller/IndexController.groovy
@@ -1,14 +1,20 @@
 package de.ollihoo.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 class IndexController {
 
+    @Value('${application.version}')
+    private String applicationVersion
+
     @RequestMapping(value="/", method = RequestMethod.GET)
-    String index() {
+    String index(Model model) {
+        model.addAttribute("applicationVersion", applicationVersion)
         "index"
     }
 }
